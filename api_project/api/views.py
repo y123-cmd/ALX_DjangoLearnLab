a35e3c492d4946c5b1624e618e332e1d14e5c6e7
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2024-11-20 21:10:54 +0300

    updated

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,12 @@
-# api/views.py
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()  # Get all books from the database
-    serializer_class = BookSerializer  # Use the BookSerializer for serialization
+class BookList(APIView):
+    def get(self, request, format=None):
+        books = Book.objects.all()  # Get all books from the database
+        serializer = BookSerializer(books, many=True)  # Serialize the list of books
+        return Response(serializer.data)  # Return the serialized data in the response
 
--- api_project/urls.py
+++
@@ -1,8 +0,0 @@
-# api_project/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Include the API URLs for the books
-]
