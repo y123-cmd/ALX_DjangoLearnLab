59cce65eb48330b8f8d3d2f3807e32de46408c1f
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2024-11-26 17:22:41 +0300

    Updated models and migrations for Author and Book, added serializers

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -10,7 +10,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    publication_date = models.DateField()  # Make sure this is added
+    publication_date = models.DateField(null=True)  # Make sure this is added
 
     def __str__(self):
         return self.title
