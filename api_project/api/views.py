a7a3c7546719600f67eeb9f16f0270906a3460ac
Yvonne <yvonnebosire24@gmail.com>
2024-11-26 16:32:03 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,12 +1,22 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import render
+
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics.ListAPIView
+from rest_framework.viewsets.ModelViewSet
 
 
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [
-        IsAuthenticated
-    ]  # Ensure that only authenticated users can access this view
+class BookList(ListAPIView):
+    queryset = Book.objects.all()  # Retrieve all Book objects
+    serializer_class = BookSerializer  # Use the BookSerializer for serialization
+
+class BookViewSet(ModelViewSet):
+    """
+    A ViewSet for handling CRUD operations for the Book model.
+    """
+    queryset = Book.objects.all()  # Retrieve all Book objects
+    serializer_class = BookSerializer  # Use the BookSerializer for data validation and representation
+    permission_classes = [IsAuthenticated]  # Restrict access to authenticated users
