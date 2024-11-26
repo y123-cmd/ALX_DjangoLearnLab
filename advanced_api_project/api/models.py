43e874ca3b98f8c3ad728fce36e8864676dc1a99
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2024-11-26 17:01:54 +0300

    Added publication_date to Book and bio to Author models

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,7 +1,10 @@
+# models.py
+
 from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
+    bio = models.TextField(blank=True, null=True)  # Optional field for bio/description of the author
 
     def __str__(self):
         return self.name
@@ -9,6 +12,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_date = models.DateField(default='2023-01-01')  # Optional field for the publication date
 
     def __str__(self):
         return self.title
