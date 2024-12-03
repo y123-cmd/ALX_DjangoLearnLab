74baa92a0e098f11770efd060ec20347d6421987
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2024-12-03 14:11:32 +0300

    updated

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = "django_blog.urls"
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "blog/static"]
+STATICFILES_DIRS = [BASE_DIR / "/static"]
 
 
 TEMPLATES = [
