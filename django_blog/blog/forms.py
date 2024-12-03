7de2ba632e3ea2b5d7368cede17f20a70082e658
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2024-12-03 21:25:19 +0300

    updated

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,18 +1,37 @@
+# blog/forms.py
 from django import forms
+from .models import Post
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Comment
+from taggit.forms import TagWidget
 
-class UserRegistrationForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ('username', 'email', 'password')
 
-class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
-
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+    
+class CommentForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ['username', 'email']
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3}),
+        }
 
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,34 +1,162 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth.models import User
 from django.contrib import messages
-from .forms import UserRegistrationForm, UserUpdateForm
+from .models import *
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import TemplateView
+from .models import Post, Comment
+from .forms import CommentForm
+from taggit.forms import TagWidget
+from django.db.models import Q
+from taggit.models import Tag
 
-# Registration view
-def register(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
+class CustomLoginView(LoginView):
+    template_name = 'blog/login.html'  # Specify the template for the login page
+
+class CustomLogoutView(LogoutView):
+    template_name = 'blog/logout.html'
+
+class HomePageView(TemplateView):
+    def register(request):
+        if request.method == 'POST':
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Registration successful.')
+                return redirect('login')
+            else:
+                messages.error(request, 'Registration failed. Please check the form.')
+        else:
+            form = CustomUserCreationForm()
+            return render(request, 'register.html', {'form': form})
+            # return HttpResponse redirect
+    
+    def login_view(request):
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            
+        if user is not None:
             login(request, user)
-            messages.success(request, "Registration successful!")
             return redirect('profile')
-    else:
-        form = UserRegistrationForm()
-    return render(request, 'registration/register.html', {'form': form})
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return render(request, 'login.html')
+        
+    def logout_view(request):
+        logout(request)
+        return redirect('login')
+    
+    @login_required
+    def profile(request):
+        if request.method == 'POST':
+            user = request.user
+            user.email = request.POST['email']
+            user.save()
+            messages.success(request, 'Profile updated successfully.')
+        return render(request, 'profile.html', {'user': request.user})
 
-# Profile view
+
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+
+class ListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+class DetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class CreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class UpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class DeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+
 @login_required
-def profile(request):
-    if request.method == 'POST':
-        form = UserUpdateForm(request.POST, instance=request.user)
+def CommentCreateView(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', post_id=post_id)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment.html', {'form': form, 'post': post})
+
+@login_required
+def CommentUpdateView(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    if request.user != comment.author:
+        return redirect('post_detail', post_id=comment.post.id)
+    if request.method == "POST":
+        form = CommentForm(request.POST, instance=comment)
         if form.is_valid():
             form.save()
-            messages.success(request, "Your profile was updated successfully!")
-            return redirect('profile')
+            return redirect('post_detail', post_id=comment.post.id)
     else:
-        form = UserUpdateForm(instance=request.user)
-    return render(request, 'profile.html', {'form': form})
+        form = CommentForm(instance=comment)
+    return render(request, 'blog/edit_comment.html', {'form': form, 'comment': comment})
 
+@login_required
+def CommentDeleteView(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    if request.user == comment.author:
+        comment.delete()
+    return redirect('post_detail', post_id=comment.post.id)
+
+def search_posts(request):
+    query = request.GET.get('q')
+    if query:
+        results = Post.objects.filter(
+            Q(title__icontains=query) | Q(content__icontains=query) | Q(tags__name__icontains=query)
+        ).distinct()
+    else:
+        results = Post.objects.none()
+
+    return render(request, 'blog/search_results.html', {'results': results, 'query': query})
+
+
+def PostByTagListView(request, tag_slug):
+    tag = get_object_or_404(Tag, slug=tag_slug)
+    posts = Post.objects.filter(tags__in=[tag])
+    return render(request, 'blog/posts_by_tag.html', {'tag': tag, 'posts': posts})
