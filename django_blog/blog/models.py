aafbefb719215d81edac97316ed3e6ad110aaa6d
Yvonne <yvonnebosire24@gmail.com>
2024-12-03 15:42:05 +0300

    Delete django_blog directory

--- django_blog/blog/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
