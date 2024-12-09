da6a7a8344da843e321cdcee4b79bbc46da3ed93
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2024-12-09 20:42:42 +0300

    initial commit of social_media_api project

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
+
+    def __str__(self):
+        return self.username
+
