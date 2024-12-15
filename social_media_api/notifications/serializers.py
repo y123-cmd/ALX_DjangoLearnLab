1812ba9ba5fdfc4bf2e94be3f26d320ca17bdd36
Yvonne <yvonnebosire24@gmail.com>
2024-12-15 15:58:18 +0300

    Delete social_media_api directory

--- social_media_api/notifications/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Notification
-
-class NotificationSerializer(ModelSerializer):
-    class Meta:
-        model = Notification
-        fields = ['actor', 'verb']
