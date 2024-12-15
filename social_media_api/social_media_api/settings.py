bde2e1ab50cb8e96ef919cedff9f7acfb82252e9
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2024-12-15 17:20:30 +0300

    updated

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -82,7 +82,8 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        'PORT': '',
     }
 }
 
