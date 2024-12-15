b5452f1ca93e08e031e4e40c669d863942de7ad8
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2024-12-15 17:25:16 +0300

    updated settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
